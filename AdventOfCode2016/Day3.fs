2591648575625b743959322e2a0cd7e5d9b5eb52
barld <bakeds@gmail.com>
2016-12-05 10:53:46 +0100

    add day3b

--- AdventOfCode2016/Day3.fs
+++ AdventOfCode2016/Day3.fs
@@ -15,7 +15,25 @@ let isPossible a b c =
 
 let parse (s:string) =
     let res = s.Split(' ') |> Array.filter (String.IsNullOrWhiteSpace >> not) |> Array.map int
-    printfn "%A" res
     isPossible res.[0] res.[1] res.[2]
 
 let answer = sInput |> Array.map parse |> Array.filter id |> Array.length
+
+//b
+let parseB (s:string) =
+    let res = s.Split(' ') |> Array.filter (String.IsNullOrWhiteSpace >> not) |> Array.map int
+    res
+
+let turn (arr:int[][]) =
+    let arrLength = arr.Length
+    let rec _loop row col =
+        if row < arrLength && col<3 then
+            isPossible arr.[row].[col] arr.[row+1].[col] arr.[row+2].[col]::_loop (row+3) col
+        else if col<3 then
+            _loop 0 (col+1)
+        else
+            []
+    _loop 0 0
+    
+
+let answerB = sInput |> Array.map parseB |> turn |> List.filter id |> List.length
